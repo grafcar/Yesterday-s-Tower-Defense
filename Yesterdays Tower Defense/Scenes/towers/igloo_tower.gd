9a219a52603beeacfda9e9f53da8ee759f520aab
seanRobC <39717209+seanRobC@users.noreply.github.com>
2024-03-17 22:57:07 -0700

    Added basic enemy, pathing functionality, and base tower

--- Yesterdays Tower Defense/Scenes/towers/igloo_tower.gd
+++ Yesterdays Tower Defense/Scenes/towers/igloo_tower.gd
@@ -5,13 +5,21 @@ var bulletDamage = 5
 var pathName
 var currTargets = []
 var curr
+var firingTimer
+var fireRate = 1.0  # Adjust this value to change the fire rate (in seconds)
+
 
 func _process(delta):
 	if is_instance_valid(curr):
 		self.look_at(curr.global_position)
-
+	else:
+		for i in get_node("BulletContainer").get_child_count():
+			get_node("BulletContainer").get_child(i).queue_free()
 
 func _on_tower_body_entered(body):
+	call_deferred("_process_on_tower_body_entered", body)
+
+func _process_on_tower_body_entered(body):
 	if "enemyDrone" in body.name:
 		var tempArray = []
 		currTargets = get_node("Tower").get_overlapping_bodies()
@@ -31,14 +39,16 @@ func _on_tower_body_entered(body):
 		curr = currTarget
 		pathName = currTarget.get_parent().name
 		
-		var tempBullet = Bullet.instantiate()
-		tempBullet.pathName = pathName
-		tempBullet.bulletDamage = bulletDamage
-		get_node("BulletContainer").add_child(tempBullet)
-		tempBullet.global_position = $Aim.global_position
-
+		self.look_at(curr.global_position)
+		
+		call_deferred("_generate_bullet", pathName)
 
 func _on_tower_body_exited(body):
 	currTargets = get_node("Tower").get_overlapping_bodies()
 
-
+func _generate_bullet(pN):
+	var tempBullet = Bullet.instantiate()
+	tempBullet.pathName = pN
+	tempBullet.bulletDamage = bulletDamage
+	get_node("BulletContainer").add_child(tempBullet)
+	tempBullet.global_position = $Aim.global_position
