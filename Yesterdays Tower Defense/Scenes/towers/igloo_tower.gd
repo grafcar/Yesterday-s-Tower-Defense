6bcea785266dd1aa77bba5061c36d8be37d53134
seanRobC <39717209+seanRobC@users.noreply.github.com>
2024-04-06 01:12:54 -0700

    Adding stable functionality for igloo towers and snowball bullets
    The timers and firerate is more realistic and usable

--- Yesterdays Tower Defense/Scenes/towers/igloo_tower.gd
+++ Yesterdays Tower Defense/Scenes/towers/igloo_tower.gd
@@ -5,50 +5,72 @@ var bulletDamage = 5
 var pathName
 var currTargets = []
 var curr
-var firingTimer
-var fireRate = 1.0  # Adjust this value to change the fire rate (in seconds)
+var fireRate = 1.4  # Adjust this value to change the fire rate (in seconds)
+var firing = false
+var loaded = true
+var bulletTimer
 
+
+func _ready():
+	bulletTimer = get_node("FiringTimer")
+	bulletTimer.wait_time = fireRate
 
 func _process(delta):
 	if is_instance_valid(curr):
 		self.look_at(curr.global_position)
+		firing = true
 	else:
 		for i in get_node("BulletContainer").get_child_count():
 			get_node("BulletContainer").get_child(i).queue_free()
+		stopFiring()
+		
+	if firing and loaded:
+		call_deferred("_generate_bullet")
+		loaded = false
+		bulletTimer.start()
+
+func updateCurrentTarget():
+	if currTargets.is_empty():
+		curr = null
+	else:
+		var furthestEnemy = null
+		var furthestProgress = -1e10 #Very large negative number
+		
+		for target in currTargets:
+			if target.name == "enemyDrone":
+				var progress = target.get_parent().get_progress()
+				if progress > furthestProgress:
+					furthestEnemy = target
+					furthestProgress = progress
+					
+		if furthestEnemy:
+			curr = furthestEnemy
+			pathName = furthestEnemy.get_parent().name
 
 func _on_tower_body_entered(body):
 	call_deferred("_process_on_tower_body_entered", body)
+	
+func _on_tower_body_exited(body):
+	call_deferred("_process_on_tower_body_exited", body)
 
 func _process_on_tower_body_entered(body):
-	if "enemyDrone" in body.name:
-		var tempArray = []
-		currTargets = get_node("Tower").get_overlapping_bodies()
-		
-		for i in currTargets:
-			if "enemyDrone" in i.name:
-				tempArray.append(i)
-		var currTarget = null
-		
-		for i in tempArray:
-			if currTarget == null:
-				currTarget = i.get_node("../")
-			else:
-				if i.get_parent().get_progress() > currTarget.get_progress():
-					currTarget = i.get_node("../")
-					
-		curr = currTarget
-		pathName = currTarget.get_parent().name
-		
-		self.look_at(curr.global_position)
-		
-		call_deferred("_generate_bullet", pathName)
+	if body.name == "enemyDrone":
+		currTargets.append(body)
+		updateCurrentTarget()
 
-func _on_tower_body_exited(body):
-	currTargets = get_node("Tower").get_overlapping_bodies()
+func _process_on_tower_body_exited(body):
+	currTargets.erase(body)
+	updateCurrentTarget()
 
-func _generate_bullet(pN):
+func _generate_bullet():
 	var tempBullet = Bullet.instantiate()
-	tempBullet.pathName = pN
 	tempBullet.bulletDamage = bulletDamage
-	get_node("BulletContainer").add_child(tempBullet)
 	tempBullet.global_position = $Aim.global_position
+	tempBullet.target = curr
+	get_node("BulletContainer").add_child(tempBullet)
+	
+func stopFiring():
+	firing = false
+
+func _on_BulletTimer_timeout():
+	loaded = true
--- Yesterdays Tower Defense/Scenes/towers/snowball.gd
+++ Yesterdays Tower Defense/Scenes/towers/snowball.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+
+var target
+var Speed = 1000
+var bulletDamage = 10
+
+func _physics_process(delta):
+	if is_instance_valid(target):
+		velocity = global_position.direction_to(target.global_position) * Speed
+		look_at(target.global_position)
+		move_and_slide()
+	else:
+		queue_free()
+
+func set_target(new_target):
+	target = new_target
+
+func _on_area_2d_body_entered(body):
+	if "enemyDrone" in body.name:
+		body.Health -= bulletDamage
+		queue_free()
