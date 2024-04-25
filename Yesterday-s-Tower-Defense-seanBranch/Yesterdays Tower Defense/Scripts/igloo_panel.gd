b40cdb46822de3ae973b8c64f19ada7db83075cb
RobertSchwartz02 <srobster22@yahoo.com>
2024-04-24 17:46:46 -0700

    Towers now cost gold and wont be placed unless sufficent gold is require, implemented tower stacking prevention with color changes if tower is placed too close to another

--- Yesterday-s-Tower-Defense-seanBranch/Yesterdays Tower Defense/Scripts/igloo_panel.gd
+++ Yesterday-s-Tower-Defense-seanBranch/Yesterdays Tower Defense/Scripts/igloo_panel.gd
@@ -6,34 +6,40 @@ var currTile
 func _on_gui_input(event):
 	
 	var tempTower = tower.instantiate()
-	if event is InputEventMouseButton and event.button_mask == 1:
+	if event is InputEventMouseButton and event.button_mask == 1 and Game.Gold >= 20:
 		add_child(tempTower)
 
 		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
-		
+		tempTower.global_position = event.global_position
 		tempTower.scale = Vector2(0.32,0.32)
 		
-	elif event is InputEventMouseMotion and event.button_mask == 1:
+	elif event is InputEventMouseMotion and event.button_mask == 1 and Game.Gold >= 20:
 		get_child(1).global_position = event.global_position
 
 		var mapPath = get_tree().get_root().get_node("Main/TileMap")
 		var tile = mapPath.local_to_map(get_global_mouse_position())
 		currTile = mapPath.get_cell_atlas_coords(0, tile, false)
+		var targets = get_child(1).get_node("TowerDetector").get_overlapping_bodies()
+		
 		if (currTile == Vector2i(4,8)):
-			get_child(1).get_node("Area").modulate = Color(0,255,0,.2)
+			if (targets.size() > 0):
+				get_child(1).get_node("Area").modulate = Color(255,0,0,.2)
+			else:
+				get_child(1).get_node("Area").modulate = Color(0,255,0,.2)
 		else:
 			get_child(1).get_node("Area").modulate = Color(255,0,0,.2)
-		
-	elif event is InputEventMouseButton and event.button_mask == 0:
+	elif event is InputEventMouseButton and event.button_mask == 0 and Game.Gold >= 20:
 		if get_child_count() > 1:
 			get_child(1).queue_free()
 		
 		if currTile == Vector2i(4,8):
 				var path = get_tree().get_root().get_node("Main/Towers")
-				path.add_child(tempTower)
-				tempTower.global_position = event.global_position
-				tempTower.get_node("Area").hide()
-				Game.Gold -= 20
+				var targets = get_child(1).get_node("TowerDetector").get_overlapping_bodies()
+				if (targets.size() < 1):
+					path.add_child(tempTower)
+					tempTower.global_position = event.global_position
+					tempTower.get_node("Area").hide()
+					Game.Gold -= 20
 		
 	else:
 		if get_child_count() > 1:
